86dba7bbd936f1ccb44b9146b96f9a6af996dab0
pmmccorkell-usna <78446270+pmmccorkell-usna@users.noreply.github.com>
2021-05-20 09:37:22 -0400

    Create user_data.py

---
+++ user_data.py
@@ -0,0 +1,10 @@
+#
+# Streamlined class of umqttsimple for micropython
+# US Naval Academy
+# Robotics and Control TSD
+# Patrick McCorkell
+# May 2021
+#
+
+#ada_user = 'YOUR USERNAME'
+#ada_key = 'YOUR API KEY'
